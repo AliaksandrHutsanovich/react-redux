aba3083bdfb87185c4ef7f6b0b8f36628c47eb76
Sanderas <guts-817@rambler.ru>
2019-08-24 23:34:07 +0300

    feat: add new functions to the file caseClickHandlers

--- app/hightOrderComponents/utils/caseClickHandlers.js
+++ app/hightOrderComponents/utils/caseClickHandlers.js
@@ -2,7 +2,21 @@ import {
   incrementInAll,
   startAddCategoryProcess,
   startAddTaskProcess,
+  startAddSubCategoryProcess,
+  startEditCategoryProcess,
+  startDeleteCategoryProcess,
 } from '../../actions/actions';
+
+const getPathParam = (path) => ({
+  savingPath: path.split('-'),
+  param: path.split('-')[path.split('-').length - 1],
+});
+
+const getSavingPath = (path) => {
+  const { savingPath } = getPathParam(path);
+  savingPath.pop();
+  return savingPath;
+};
 
 export const caseClickHandlers = {
   'Add new category': (dispatch, value, addAction) => {
@@ -21,4 +35,23 @@ export const caseClickHandlers = {
 export const kindsOfAddActions = {
   'Add new category': startAddCategoryProcess,
   'Add new task': startAddTaskProcess,
+};
+
+export const typesCategoryOperation = {
+  'Add new subcategory': (path, dispatch, value) => {
+    dispatch(startAddSubCategoryProcess({ path: (path + '-subCategories').split('-'), title: value }));
+  },
+  'Edit category': (path, dispatch, value, title) => {
+    dispatch(startEditCategoryProcess({
+      path: getSavingPath(path),
+      pathParam: getPathParam(path).param,
+      title: value || title,
+    }));
+  },
+  'Delete category': (path, dispatch) => {
+    dispatch(startDeleteCategoryProcess({
+      path: getSavingPath(path),
+      pathParam: getPathParam(path).param,
+    }));
+  },
 };

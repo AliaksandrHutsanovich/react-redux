4868d644387796f46284a56a4f4096883f89a161
Sanderas <guts-817@rambler.ru>
2019-08-24 23:34:49 +0300

    fix: refactor for eslint validation

--- app/hightOrderComponents/changingDataModal.js
+++ app/hightOrderComponents/changingDataModal.js
@@ -1,61 +1,66 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Modal, Button, Input } from 'antd';
-import { addSubCategory, deleteCategory, editCategory } from '../actions/actions';
+import { Modal, Input } from 'antd';
 import { connect } from 'react-redux';
-import { startAddSubCategoryProcess, startEditCategoryProcess, startDeleteCategoryProcess, clearReDo } from '../actions/actions';
+import PropTypes from 'prop-types';
+import { clearReDo } from '../actions/actions';
+import { typesCategoryOperation } from './utils/caseClickHandlers';
 
 function changingDataDialog(operationTitle) {
-    class ChangingDataDialog extends React.Component {
-        constructor(props) {
-            super(props);
-        };
-
-        state = {value: ""};
-
-        handleChange = (e) => {
-            this.setState({value: e.target.value});
-        }
-
-
-        handleClickOk = (handleOk, path, dispatch, title) => {
-            dispatch(clearReDo());
-            if (operationTitle === 'Add new subcategory') {
-                dispatch(startAddSubCategoryProcess({path: (path + "-subCategories").split('-'), title: this.state.value}));
-            }
-            else {
-                path = path.split('-');
-                let lenght = path.length;
-                let pathParam = path[lenght - 1];
-                path.splice(length - 1, 1);
-
-                if (operationTitle === 'Edit category') {
-                    dispatch(startEditCategoryProcess({path: path, pathParam: pathParam, title: this.state.value || title}));
-                }
-                else {
-                    dispatch(startDeleteCategoryProcess({ path: path, pathParam: pathParam }));
-                }
-            }
-            this.setState({ value: "" });
-            handleOk();
-        }
-    
-        render() {
-            let { dispatch, title } = this.props;
-            return (
-                <Modal
-                   title={operationTitle}
-                   visible={this.props.visible}
-                   onOk={() => this.handleClickOk(this.props.handleOk, this.props.path, dispatch, title)}
-                   onCancel={this.props.handleCancel}
-                >
-                    {operationTitle === "Delete category?" ? <p>{this.props.titleCategory}</p> :<Input placeholder="input category title" defaultValue={title} value={this.state.value || title} onChange={this.handleChange} className="modal-input" />}
-                </Modal>
-            );
-        }
+  class ChangingDataDialog extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { value: '' };
     }
 
-    return connect()(ChangingDataDialog);
+    handleChange = (e) => {
+      this.setState({ value: e.target.value });
+    }
+
+    handleClickOk = (handleOk, path, dispatch, title) => {
+      const { value } = this.state;
+      dispatch(clearReDo());
+      typesCategoryOperation[operationTitle](path, dispatch, value, title);
+      this.setState({ value: '' });
+      handleOk();
+    }
+
+    render() {
+      const {
+        dispatch,
+        title,
+        visible,
+        handleOk,
+        path,
+        titleCategory,
+        value,
+        handleCancel,
+      } = this.props;
+      return (
+        <Modal
+          title={operationTitle}
+          visible={visible}
+          onOk={() => this.handleClickOk(handleOk, path, dispatch, title)}
+          onCancel={handleCancel}
+        >
+          {operationTitle === 'Delete category?' ? <p>{titleCategory}</p> : <Input placeholder="input category title" defaultValue={title} value={value || title} onChange={this.handleChange} className="modal-input" />}
+        </Modal>
+      );
+    }
+  }
+
+  ChangingDataDialog.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    title: PropTypes.string.isRequired,
+    visible: PropTypes.bool.isRequired,
+    handleOk: PropTypes.func.isRequired,
+    path: PropTypes.string.isRequired,
+    titleCategory: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    handleCancel: PropTypes.func.isRequired,
+  };
+
+  return connect()(ChangingDataDialog);
 }
 
 export default changingDataDialog;

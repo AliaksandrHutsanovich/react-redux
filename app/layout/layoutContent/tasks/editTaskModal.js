5e796cd07c207f8818df9990675d8956b7636e69
Sanderas <guts-817@rambler.ru>
2019-08-26 00:10:24 +0300

    feat: create new file utils.js

--- app/layout/layoutContent/tasks/editTaskModal.js
+++ app/layout/layoutContent/tasks/editTaskModal.js
@@ -1,105 +1,155 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { 
-    Modal, Button, Input, Form, Checkbox, Row, Col 
+import {
+  Modal, Input, Form, Checkbox, Row, Col,
 } from 'antd';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import treeOfCategories from '../../../hightOrderComponents/treeOfCategories';
 import CategoryItem from './categoryItem';
-import { connect } from 'react-redux';
-import { editTask } from '../../../actions/actions';
-import { startEditTaskProcess, clearReDo } from '../../../actions/actions';
-import { incrementInDone, decrementInDone } from '../../../actions/actions';
+import {
+  startEditTaskProcess,
+  clearReDo,
+} from '../../../actions/actions';
+import getStateValue, { getOldPathParams } from './utils/utils';
 
-const {TextArea} = Input;
+const { TextArea } = Input;
 const Categories = treeOfCategories(CategoryItem);
 
 class EditTaskModal extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedPath: '',
+      title: '',
+      description: '',
+      isFinished: '',
+      isStatusChanged: false,
+    };
+  }
+
+  onSelectCategory(e) {
+    this.setState({ selectedPath: e[0] });
+  }
+
+  onSaveTask(
+    e,
+    dispatch,
+    newPath,
+    oldPath,
+    title,
+    description,
+    isFinished,
+    handleOk,
+    oldPathParam,
+    newPathParam,
+  ) {
+    dispatch(clearReDo());
+    const {
+      isStatusChanged,
+      isFinishedTask,
+      title: stateTitle,
+      description: stateDescription,
+      isFinished: stateIsFinished,
+    } = this.state;
+    dispatch(startEditTaskProcess({
+      newPath,
+      oldPath,
+      oldPathParam,
+      newPathParam,
+      title: stateTitle || title,
+      description: stateDescription || description,
+      isFinished: stateIsFinished === '' ? isFinished : getStateValue(isFinishedTask, isStatusChanged, dispatch),
+    }));
+    handleOk();
+  }
+
+  onChangeTaskStatus(e, isFinished) {
+    if (e.target.checked === isFinished) {
+      this.setState({ isFinished: e.target.checked, isStatusChanged: false });
+    } else {
+      this.setState({ isFinished: e.target.checked, isStatusChanged: true });
     }
+  }
 
-    state = { selectedPath: '', title: '', description: '', isFinished: '', isStatusChanged: false }
+  onChangeTaskDescription(e) { this.setState({ description: e.target.value }); }
 
-    onSelectCategory = (e) => {
-        this.setState({selectedPath: e[0]});
-    }
-
-    onSaveTask = (e, dispatch, newPath, oldPath, title, description, isFinished, handleOk, oldPathParam, newPathParam) => {
-        dispatch(clearReDo());
-        let isStatusChanged = this.state.isStatusChanged;
-        let isFinishedTask = this.state.isFinished;
-
-        function getStateValue() {
-            if (isStatusChanged) {
-                isFinishedTask ? dispatch(incrementInDone()) : dispatch(decrementInDone());
-            }
-            return isFinishedTask;
-        }
-
-        dispatch(startEditTaskProcess({ 
-            newPath: newPath, 
-            oldPath: oldPath,
-            oldPathParam: oldPathParam,
-            newPathParam: newPathParam,
-            title: this.state.title || title, 
-            description: this.state.description || description, 
-            isFinished: this.state.isFinished === '' ? isFinished : getStateValue()
-        }));
-        handleOk();
-    }
-
-    onChangeTaskStatus = (e, isFinished) => { 
-        if (e.target.checked === isFinished) {
-            this.setState({ isFinished: e.target.checked, isStatusChanged: false });
-        }
-        else {
-            this.setState({ isFinished: e.target.checked, isStatusChanged: true });
-        }
-    }
-    onChangeTaskDescription = (e) => this.setState({ description: e.target.value });
-    onChangeTaskTitle = (e) => this.setState({ title: e.target.value });
+  onChangeTaskTitle(e) { this.setState({ title: e.target.value }); }
 
 
-    render() {
-        let { taskTitle, description, isFinished, oldPath, handleOk, dispatch } = this.props;
+  render() {
+    const {
+      taskTitle,
+      description,
+      isFinished,
+      oldPath,
+      handleOk,
+      dispatch,
+      visible,
+      handleCancel,
+    } = this.props;
+    const { selectedPath } = this.state;
+    const oldPathParams = getOldPathParams(oldPath.split('-'));
+    const newPath = selectedPath ? (selectedPath + '-tasks').split('-') : '';
+    const newPathParam = '';
 
-        oldPath = oldPath.split('-');
-        let oldPathParam = oldPath[oldPath.length - 1];
-        oldPath = oldPath.filter((x, index, oldPath) => index < oldPath.length - 1);
-        let { selectedPath, title } = this.state;
-        let newPath = selectedPath ? (selectedPath + '-tasks').split('-') : '';
-        let newPathParam = '';
-
-        return (
-            <Modal 
-                title="Edit task"
-                visible={this.props.visible}
-                onOk={(e) => this.onSaveTask(e, dispatch, newPath, oldPath, taskTitle, description, isFinished, handleOk, oldPathParam, newPathParam )}
-                onCancel={this.props.handleCancel}
-                okText="Save changes"
-                width="900px">
-                <Form>
-                    <Row>
-                        <Col span={12}>
-                            <Categories onSelectCategory={this.onSelectCategory} />
-                        </Col>
-                        <Col span={12}>
-                            <Row>
-                                <Input defaultValue={taskTitle} placeholder="Input task title" onChange={this.onChangeTaskTitle} required />
-                            </Row>
-                            <Row>
-                                <Checkbox defaultChecked={isFinished} onChange={this.onChangeTaskStatus}>Done</Checkbox>
-                            </Row>
-                            <Row>
-                                <TextArea rows={4} defaultValue={description} onChange={this.onChangeTaskDescription} placeholder="Input type description" />
-                            </Row>
-                        </Col>
-                    </Row>
-                </Form>
-            </Modal>
-        );
-    }
+    return (
+      <Modal
+        title="Edit task"
+        visible={visible}
+        onOk={(e) => this.onSaveTask(
+          e,
+          dispatch,
+          newPath,
+          oldPathParams.oldPath,
+          taskTitle,
+          description,
+          isFinished,
+          handleOk,
+          oldPathParams.oldPathParam,
+          newPathParam,
+        )}
+        onCancel={handleCancel}
+        okText="Save changes"
+        width="900px"
+      >
+        <Form>
+          <Row>
+            <Col span={12}>
+              <Categories onSelectCategory={this.onSelectCategory} />
+            </Col>
+            <Col span={12}>
+              <Row>
+                <Input defaultValue={taskTitle} placeholder="Input task title" onChange={this.onChangeTaskTitle} required />
+              </Row>
+              <Row>
+                <Checkbox
+                  defaultChecked={isFinished}
+                  onChange={this.onChangeTaskStatus}
+                >
+                  Done
+                </Checkbox>
+              </Row>
+              <Row>
+                <TextArea rows={4} defaultValue={description} onChange={this.onChangeTaskDescription} placeholder="Input type description" />
+              </Row>
+            </Col>
+          </Row>
+        </Form>
+      </Modal>
+    );
+  }
 }
 
-export default connect()(EditTaskModal);
+EditTaskModal.propTypes = {
+  taskTitle: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  isFinished: PropTypes.bool.isRequired,
+  oldPath: PropTypes.string.isRequired,
+  handleOk: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  visible: PropTypes.bool.isRequired,
+  handleCancel: PropTypes.func.isRequired,
+};
 
+export default connect()(EditTaskModal);

c26313d0bfa3e45082366ca72fa4f593461e7af3
Sanderas <guts-817@rambler.ru>
2019-08-25 22:37:35 +0300

    fix: fix eslint validation

--- app/layout/layoutContent/filtersElems/filters.js
+++ app/layout/layoutContent/filtersElems/filters.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Checkbox } from 'antd';
-import { Input } from 'antd';
-import { showDoneTasks, searchTasks } from '../../../actions/actions';
+import { Checkbox, Input } from 'antd';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { showDoneTasks, searchTasks } from '../../../actions/actions';
 
 
 class Filters extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  onChange = (e, dispatch) => {
+    dispatch(showDoneTasks(e.target.checked));
+  }
 
-    onChange = (e, dispatch) => {
-        dispatch(showDoneTasks(e.target.checked));
-    }
+  onChangePattern = (e, dispatch) => {
+    dispatch(searchTasks(e.target.value));
+  }
 
-    onChangePattern = (e, dispatch) => {
-        dispatch(searchTasks(e.target.value));
-    }
-
-    render() {
-        let { dispatch, searchKey, showDone } = this.props;
-        return (
-          <div className="filters">
-              <Checkbox defaultChecked={showDone} onClick={(e) => this.onChange(e, dispatch)}>
-                  Show done
-              </Checkbox>
-              <Input
-                  placeholder="search"
-                  style={{ width: 200 }}
-                  defaultValue={searchKey}
-                  onChange={(e) => this.onChangePattern(e, dispatch)}
-               />
-          </div>
-        );
-    }
+  render() {
+    const { dispatch, searchKey, showDone } = this.props;
+    return (
+      <div className="filters">
+        <Checkbox defaultChecked={showDone} onClick={(e) => this.onChange(e, dispatch)}>
+          Show done
+        </Checkbox>
+        <Input
+          placeholder="search"
+          style={{ width: 200 }}
+          defaultValue={searchKey}
+          onChange={(e) => this.onChangePattern(e, dispatch)}
+        />
+      </div>
+    );
+  }
 }
+
+Filters.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  searchKey: PropTypes.string.isRequired,
+  showDone: PropTypes.bool.isRequired,
+};
 
 export default connect()(Filters);

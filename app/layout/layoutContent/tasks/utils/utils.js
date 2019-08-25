55a1032cb7ee8d96660d49bbacfd7e4bf88b4a4f
Sanderas <guts-817@rambler.ru>
2019-08-26 00:12:03 +0300

    feat: create new file util.js and in previous commit title a mistake

---
+++ app/layout/layoutContent/tasks/utils/utils.js
@@ -0,0 +1,23 @@
+import {
+  incrementInDone,
+  decrementInDone,
+} from '../../../../actions/actions';
+
+const changeInDone = (
+  isFinishedTask,
+  dispatch,
+) => (isFinishedTask ? dispatch(incrementInDone()) : dispatch(decrementInDone()));
+
+const getStateValue = (isFinishedTask, isStatusChanged, dispatch) => {
+  if (isStatusChanged) {
+    changeInDone(isFinishedTask, dispatch);
+  }
+  return isFinishedTask;
+};
+
+export const getOldPathParams = (oldPath) => ({
+  oldPath: oldPath.filter((x, index, path) => index < path.length - 1),
+  oldPathParam: oldPath[oldPath.length - 1],
+});
+
+export default getStateValue;

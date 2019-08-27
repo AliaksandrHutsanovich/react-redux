8a02051a79e70af2bcdab540b005719bcb7d5787
Sanderas <guts-817@rambler.ru>
2019-08-27 21:12:28 +0300

    fix: eslint validation in the file layoutSiderContent.js

--- app/layout/layoutSider/layoutSiderContent.js
+++ app/layout/layoutSider/layoutSiderContent.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import addForm from '../../hightOrderComponents/addForm';
-import Category from '../layoutSider/categories/category';
+import Category from './categories/category';
 import treeOfCategories from '../../hightOrderComponents/treeOfCategories';
 
 const Categories = treeOfCategories(Category);
 
-const AddCategory = addForm("Add new category"); 
+const AddCategory = addForm('Add new category');
 
 class LayoutSiderContent extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                <AddCategory />
-                <Categories />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <AddCategory />
+        <Categories />
+      </div>
+    );
+  }
 }
 
 export default LayoutSiderContent;

fdfd91d70acf04753b0a5235f35ca4634fdc97b1
Sanderas <guts-817@rambler.ru>
2019-09-01 18:30:07 +0300

    fix: eslint validation in appView.js

--- app/appView.js
+++ app/appView.js
@@ -1,75 +1,94 @@
+/*
+  eslint react/no-array-index-key: 0,
+  react/jsx-boolean-value: 0,
+  no-shadow: 0
+*/
+
 import React from 'react';
 import 'antd/dist/antd.css';
 import '../styles/styles.css';
-import { Progress } from 'antd';
+import PropTypes from 'prop-types';
 import { Layout } from 'antd';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import HeaderContent from './header/headerContent';
 import LayoutSiderContent from './layout/layoutSider/layoutSiderContent';
 import LayoutContent from './layout/layoutContent/layoutContent';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
 
-import { getTasks, getUrls } from './selectors/selectors';
-import { createSelector } from 'reselect';
-
+import { getUrls } from './selectors/selectors';
 
 const {
-    Header, Footer, Sider, Content,
+    Header, Sider, Content,
   } = Layout;
 
 class AppView extends React.Component {
-
-    shouldComponentUpdate(nextProps) {
-        return !(this.props.showDone === nextProps.showDone && 
-                 this.props.searchKey === nextProps.searchKey && 
-                 this.props.urls.length === nextProps.urls.length);
+  shouldComponentUpdate(nextProps) {
+    const {
+      showDone,
+      searchKey,
+      urls: {
+        length,
+      },
+    } = this.props;
+    return !(showDone === nextProps.showDone
+      && searchKey === nextProps.searchKey
+      && length === nextProps.urls.length);
     }
 
     render() {
-        console.log('Appview');
-        let {taskGroups,showDone,searchKey,dispatch} = this.props;
-        let routes = this.props.urls.map((url, index) => 
-                               <Route 
-                                  key={index} 
-                                  path={`/${url}`} 
-                                  render={(props) => { 
-                                      return <LayoutContent {...props} url={url} showDone={showDone} searchKey={searchKey} />;
-                                    }}
-                                />);
-        return (
-            <BrowserRouter>
-                <Layout className="wrapper">
-                    <Header className="header">
-                       <HeaderContent />
-                    </Header>
-                    <Layout className="wrapper__main" hasSider={true}>
-                       <Sider className="wrapper__sider" width="30%">
-                           <LayoutSiderContent />
-                       </Sider>
-                       <Content className="wrapper__content">
-                           {routes}
-                       </Content>
-                    </Layout>
-                </Layout>
-            </BrowserRouter>
-        );
+      const { showDone, searchKey, urls } = this.props;
+      const routes = urls.map((url, index) => (
+        <Route
+          key={index}
+          path={`/${url}`}
+          render={(props) => (
+            <LayoutContent {...props} url={url} showDone={showDone} searchKey={searchKey} />
+          )}
+        />
+      ));
+      return (
+        <BrowserRouter>
+          <Layout className="wrapper">
+            <Header className="header">
+              <HeaderContent />
+            </Header>
+            <Layout className="wrapper__main" hasSider={true}>
+              <Sider className="wrapper__sider" width="30%">
+                <LayoutSiderContent />
+              </Sider>
+              <Content className="wrapper__content">
+                {routes}
+              </Content>
+            </Layout>
+          </Layout>
+        </BrowserRouter>
+      );
     }
 }
 
-const urls = state => getUrls(state);
-const showDone = state => state.filters.get('showDone');
-const searchKey = state => state.filters.get('searchKey');
+const urls = (state) => getUrls(state);
+const showDone = (state) => state.filters.get('showDone');
+const searchKey = (state) => state.filters.get('searchKey');
 
-const urlSelector = createSelector(urls, url => url);
-const showDoneSelector = createSelector(showDone, showDone => showDone);
-const searchKeySelector = createSelector(searchKey, searchKey => searchKey);
+const urlSelector = createSelector(urls, (url) => url);
+const showDoneSelector = createSelector(showDone, (showDone) => showDone);
+const searchKeySelector = createSelector(searchKey, (searchKey) => searchKey);
 
-const mapStateToProps = (state) => {
-    return {
-      urls: urlSelector(state),
-      showDone: showDoneSelector(state),
-      searchKey: searchKeySelector(state)
-    }
-}
+const mapStateToProps = (state) => ({
+  urls: urlSelector(state),
+  showDone: showDoneSelector(state),
+  searchKey: searchKeySelector(state),
+});
+
+AppView.defaultProps = {
+  searchKey: '',
+};
+
+AppView.propTypes = {
+  showDone: PropTypes.bool.isRequired,
+  searchKey: PropTypes.string,
+  urls: PropTypes.arrayOf().isRequired,
+};
 
 export default connect(mapStateToProps)(AppView);

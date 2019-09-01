dc6eb5fe104eb85826496e1eae095f7d5077aaca
Sanderas <guts-817@rambler.ru>
2019-09-01 14:45:47 +0300

    fix: fix eslint validation in file editCategory.js

--- app/sagas/editCategory.js
+++ app/sagas/editCategory.js
@@ -2,16 +2,26 @@ import { takeEvery, put, select } from 'redux-saga/effects';
 import { editCategory, addToUnDo, editCategoryRevive } from '../actions/actions';
 import { getEntity } from '../selectors/selectorsForEntities';
 
-export function* editCategoryWatcher() {
-    yield takeEvery('START_EDIT_CATEGORY_PROCESS', editCategoryGen);
+function* editCategoryGen(action) {
+  const category = yield select(getEntity(
+    action.payload.path,
+    action.payload.pathParam,
+    action.payload.title,
+  ));
+  yield put(editCategory({
+    path: action.payload.path,
+    pathParam: action.payload.pathParam,
+    title: action.payload.title,
+  }));
+  yield put(addToUnDo({
+    undoOperation: editCategory,
+    redoOperation: editCategoryRevive,
+    obj: category,
+  }));
 }
 
-function* editCategoryGen(action) {
-    let category = yield select(getEntity(action.payload.path, action.payload.pathParam, action.payload.title));
-    yield put(editCategory({ path: action.payload.path, pathParam: action.payload.pathParam, title: action.payload.title }));
-    yield put(addToUnDo({
-        undoOperation: editCategory,
-        redoOperation: editCategoryRevive,
-        obj: category
-    }));
+function* editCategoryWatcher() {
+  yield takeEvery('START_EDIT_CATEGORY_PROCESS', editCategoryGen);
 }
+
+export default editCategoryWatcher;

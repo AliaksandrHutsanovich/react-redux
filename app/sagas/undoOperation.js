d02c4b6f0237995db9a0ae7504d07a79619a3915
Sanderas <guts-817@rambler.ru>
2019-09-01 15:01:26 +0300

    fix: fix eslint validation in file undoOperaion.js

--- app/sagas/undoOperation.js
+++ app/sagas/undoOperation.js
@@ -2,15 +2,17 @@ import { takeEvery, put, select } from 'redux-saga/effects';
 import { deleteFromUnDo, addToReDo } from '../actions/actions';
 import { getUndoOperation, getObjFromUndoOperation, getObjWrapperFromUndoOperation } from '../selectors/selectorsForOperation';
 
-export function* undoOperationWatcher() {
-    yield takeEvery('START_UNDO_PROCESS', undoOperationGen);
+function* undoOperationGen() {
+  const operation = yield select(getUndoOperation);
+  const obj = yield select(getObjFromUndoOperation);
+  yield put(operation(obj));
+  const objWrapper = yield select(getObjWrapperFromUndoOperation);
+  yield put(addToReDo(objWrapper));
+  yield put(deleteFromUnDo());
 }
 
-function* undoOperationGen() {
-    let operation = yield select(getUndoOperation);
-    let obj = yield select(getObjFromUndoOperation);
-    yield put(operation(obj));
-    let objWrapper = yield select(getObjWrapperFromUndoOperation);
-    yield put(addToReDo(objWrapper));
-    yield put(deleteFromUnDo());
+function* undoOperationWatcher() {
+  yield takeEvery('START_UNDO_PROCESS', undoOperationGen);
 }
+
+export default undoOperationWatcher;

bb6903e5b0615fe272f49f807c4d40d9c6a88961
Sanderas <guts-817@rambler.ru>
2019-09-01 13:25:34 +0300

    fix: fix eslint validation in reducer.js

--- app/reducers/reducer.js
+++ app/reducers/reducer.js
@@ -1,22 +1,14 @@
 import { combineReducers } from 'redux';
-import {actionReducers} from './actionReducers'; 
-import {filters} from  './filters';
-import { countPerсent } from './countPercent';
-import { reDoReducer } from './reDoReducer';
-import { unDoReducer } from './unDoReducer';
+import actionReducers from './actionReducers';
+import filters from './filters';
+import countPerсent from './countPercent';
+import reDoReducer from './reDoReducer';
+import unDoReducer from './unDoReducer';
 
 export default combineReducers({
-    actionReducers,
-    filters,
-    countPerсent,
-    reDoReducer,
-    unDoReducer
+  actionReducers,
+  filters,
+  countPerсent,
+  reDoReducer,
+  unDoReducer,
 });
-
-
-
-
-
-
-
-

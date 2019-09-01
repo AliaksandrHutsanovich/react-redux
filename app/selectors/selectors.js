2af7622a037c7f140c91fcd0ba4ed395644c4e7f
Sanderas <guts-817@rambler.ru>
2019-09-01 17:21:20 +0300

    fix: refactor file app/selectors/selectors.js

--- app/selectors/selectors.js
+++ app/selectors/selectors.js
@@ -1,23 +1,24 @@
+const getSubCategories = (key, subCategories) => {
+  let arrSubCategories = [];
+  if (subCategories.length > 0) {
+    arrSubCategories = subCategories.map((subCategory, num) => {
+      const subCategoryElem = subCategory.toObject();
+      subCategoryElem.key = key + '-subCategories-' + num;
+      subCategoryElem.subCategories = subCategoryElem.subCategories.toArray();
+      if (subCategoryElem.subCategories.length > 0) {
+        subCategoryElem.subCategories = getSubCategories(
+          subCategoryElem.key,
+          subCategoryElem.subCategories,
+        );
+      }
+      return subCategoryElem;
+    });
+  }
+  return arrSubCategories;
+};
+
 export const getCategories = (state) => {
   const categories = state.actionReducers.get('categories').toArray();
-  const getSubCategories = (key, subCategories) => {
-    let arrSubCategories = [];
-    if (subCategories.length > 0) {
-      arrSubCategories = subCategories.map((subCategory, num) => {
-        const subCategoryElem = subCategory.toObject();
-        subCategoryElem.key = key + '-subCategories-' + num;
-        subCategoryElem.subCategories = subCategoryElem.subCategories.toArray();
-        if (subCategoryElem.subCategories.length > 0) {
-          subCategoryElem.subCategories = getSubCategories(
-            subCategoryElem.key,
-            subCategoryElem.subCategories,
-          );
-        }
-        return subCategoryElem;
-      });
-    }
-    return arrSubCategories;
-  };
 
   const arrCategories = categories.map((category, num) => {
     const categoryElem = category.toObject();
@@ -40,13 +41,18 @@ export const getTasks = (state) => {
     if (subCategories.length > 0) {
       subCategories.forEach((subCategory, num) => {
         const subCategoryObj = subCategory.toObject();
-        subCategoryObj.key = key + '-subCategories-' + num;
-        urlsArr.push(subCategoryObj.key);
-        subCategoryObj.tasks = subCategoryObj.tasks.toArray();
-        arrayOfTaskGroups.push(subCategoryObj.tasks.map((task) => task.toObject()));
-        subCategoryObj.subCategories = subCategoryObj.subCategories.toArray();
-        if (subCategoryObj.subCategories.length > 0) {
-          getTasksFun(subCategoryObj.key, subCategoryObj.subCategories);
+        urlsArr.push(key + '-subCategories-' + num);
+
+        arrayOfTaskGroups.push(
+          subCategoryObj.tasks.toArray()
+            .map((task) => task.toObject()),
+        );
+
+        if (subCategoryObj.subCategories.toArray().length > 0) {
+          getTasksFun(
+            key + '-subCategories-' + num,
+            subCategoryObj.subCategories.toArray(),
+          );
         }
       });
     }
@@ -54,12 +60,19 @@ export const getTasks = (state) => {
 
   categories.forEach((category, num) => {
     const categoryObj = category.toObject();
-    categoryObj.key = 'categories-' + num;
-    urlsArr.push(categoryObj.key);
-    categoryObj.tasks = categoryObj.tasks.toArray();
-    arrayOfTaskGroups.push(categoryObj.tasks.map((task) => task.toObject()));
-    categoryObj.subCategories = categoryObj.subCategories.toArray();
-    getTasksFun(categoryObj.key, categoryObj.subCategories);
+    urlsArr.push('categories-' + num);
+
+    arrayOfTaskGroups.push(
+      categoryObj.tasks.toArray()
+        .map(
+          (task) => task.toObject(),
+        ),
+    );
+
+    getTasksFun(
+      'categories-' + num,
+      categoryObj.subCategories.toArray(),
+    );
   });
 
   return {
@@ -71,25 +84,30 @@ export const getTasks = (state) => {
 export const getUrls = (state) => {
   const categories = state.actionReducers.get('categories').toArray();
     const urls = [];
+
     const getTasksFun = (key, subCategories) => {
       if (subCategories.length > 0) {
         subCategories.forEach((subCategory, num) => {
           const subCategoryObj = subCategory.toObject();
-          subCategoryObj.key = key + '-subCategories-' + num;
-          urls.push(subCategoryObj.key);
-          subCategoryObj.subCategories = subCategoryObj.subCategories.toArray();
-          if (subCategoryObj.subCategories.length > 0) {
-            getTasksFun(subCategoryObj.key, subCategoryObj.subCategories);
+          urls.push(key + '-subCategories-' + num);
+          if (subCategoryObj.subCategories.toArray().length > 0) {
+            getTasksFun(
+              key + '-subCategories-' + num,
+              subCategoryObj.subCategories.toArray(),
+            );
           }
         });
       }
     };
+
     categories.forEach((category, num) => {
       const categoryObj = category.toObject();
       categoryObj.key = 'categories-' + num;
-      urls.push(categoryObj.key);
-      categoryObj.subCategories = categoryObj.subCategories.toArray();
-      getTasksFun(categoryObj.key, categoryObj.subCategories);
+      urls.push('categories-' + num);
+      getTasksFun(
+        'categories-' + num,
+        categoryObj.subCategories.toArray(),
+      );
     });
 
     return urls;
--- app/selectors/selectorsForEntities.js
+++ app/selectors/selectorsForEntities.js
@@ -1,44 +1,51 @@
-export const getEntity = (path, pathParam, titlePrimary) => state => {
-    let fullPath = Object.assign([], path);
-    fullPath.push(pathParam);
-    let entity = state.actionReducers.getIn(fullPath).toObject();
-    entity.path = path;
-    entity.pathParam = pathParam;
-    if (titlePrimary) {
-        entity.titlePrimary = titlePrimary;
-    }
-    return entity;
-}
+export const getEntity = (path, pathParam, titlePrimary) => (state) => {
+  const fullPath = Object.assign([], path);
+  fullPath.push(pathParam);
+  const entity = state.actionReducers.getIn(fullPath).toObject();
+  entity.path = path;
+  entity.pathParam = pathParam;
+  if (titlePrimary) {
+    entity.titlePrimary = titlePrimary;
+  }
+  return entity;
+};
 
-export const getEntityByPath = (oldPath, newPath, newTitle, newDescription, newIsFinishedValue, oldPathParam, newPathParam) => state => {
-    let fullOldPath = Object.assign([], oldPath);
-    fullOldPath.push(oldPathParam);
-    let entity = state.actionReducers.getIn(fullOldPath).toObject();
-    if (newPath) {
-        entity.newPath = oldPath;
-        entity.newPathParam = oldPathParam;
-        entity.oldPath = newPath;
-        if (!newPathParam) {
-            newPathParam = state.actionReducers.getIn(newPath).toArray().length;
-        }
-        entity.oldPathParam = newPathParam;
+export const getEntityByPath = (
+  oldPath,
+  newPath,
+  newTitle,
+  newDescription,
+  newIsFinishedValue,
+  oldPathParam,
+  newPathParam,
+) => (state) => {
+  const fullOldPath = Object.assign([], oldPath);
+  fullOldPath.push(oldPathParam);
+  const entity = state.actionReducers.getIn(fullOldPath).toObject();
+  if (newPath) {
+    entity.newPath = oldPath;
+    entity.newPathParam = oldPathParam;
+    entity.oldPath = newPath;
+    let newPathParamVar = newPathParam;
+    if (!newPathParamVar) {
+      newPathParamVar = state.actionReducers.getIn(newPath).toArray().length;
     }
-    else {
-        entity.newPath = newPath;
-        entity.newPathParam = newPathParam;
-        entity.oldPath = oldPath;
-        entity.oldPathParam = oldPathParam;
-    }
-    entity.description = entity.Description;
-    if (newTitle) {
-        entity.titlePrimary = newTitle;
-    }
-    if (newDescription) {
-        entity.descriptionPrimary = newDescription;
-    }
-    if (newIsFinishedValue) {
-        entity.isFinishedValuePrimary = newIsFinishedValue;
-    }
-    return entity;
-}
-
+    entity.oldPathParam = newPathParamVar;
+  } else {
+    entity.newPath = newPath;
+    entity.newPathParam = newPathParam;
+    entity.oldPath = oldPath;
+    entity.oldPathParam = oldPathParam;
+  }
+  entity.description = entity.Description;
+  if (newTitle) {
+    entity.titlePrimary = newTitle;
+  }
+  if (newDescription) {
+    entity.descriptionPrimary = newDescription;
+  }
+  if (newIsFinishedValue) {
+    entity.isFinishedValuePrimary = newIsFinishedValue;
+  }
+  return entity;
+};

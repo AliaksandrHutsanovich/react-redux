24e461997d90cfee1754f7705807d0c281b12c4a
Sanderas <guts-817@rambler.ru>
2019-09-01 18:05:02 +0300

    fix: fix eslint validation in file app/selectors/selectorsForPaths.js

--- app/selectors/selectorsForPaths.js
+++ app/selectors/selectorsForPaths.js
@@ -1,5 +1,7 @@
-export const getCategoryPath = state => 'categories-' + (state.actionReducers.get("categories").toArray().length - 1);
+export const getCategoryPath = (state) => 'categories-' + (state.actionReducers.get('categories').toArray().length - 1);
 
-export const getSubCategoryPath = path => state => (state.actionReducers.getIn(path).toArray().length - 1); 
+export const getSubCategoryPath = (path) => (state) => state
+  .actionReducers.getIn(path).toArray().length - 1;
 
-export const getTaskPath = path => state => (state.actionReducers.getIn(path).toArray().length - 1);
+export const getTaskPath = (path) => (state) => state
+  .actionReducers.getIn(path).toArray().length - 1;

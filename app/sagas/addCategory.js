675a2f0baeb54325b60daaafa839d931ad4c7eec
Sanderas <guts-817@rambler.ru>
2019-09-01 13:31:11 +0300

    fix: fixeslint validation in file addCategory.js

--- app/sagas/addCategory.js
+++ app/sagas/addCategory.js
@@ -2,16 +2,24 @@ import { takeEvery, put, select } from 'redux-saga/effects';
 import { addCategory, deleteCategory, addToUnDo } from '../actions/actions';
 import { getCategoryPath } from '../selectors/selectorsForPaths';
 
-export function* addCategoryWatcher() {
-    yield takeEvery('START_ADD_CATEGORY_PROCESS', addCategoryGen);
+function* addCategoryGen(action) {
+  yield put(addCategory({ title: action.payload.title }));
+  const path = yield select(getCategoryPath);
+  yield put(addToUnDo({
+    undoOperation: deleteCategory,
+    redoOperation: addCategory,
+    obj: {
+      path: [path.split('-')[0]],
+      pathParam: path.split('-')[1],
+      title: action.payload.title,
+      subCategories: [],
+      tasks: [],
+    },
+  }));
 }
 
-function* addCategoryGen(action) {
-    yield put(addCategory({ title: action.payload.title }));
-    let path = yield select(getCategoryPath);
-    yield put(addToUnDo({ 
-        undoOperation: deleteCategory, 
-        redoOperation: addCategory, 
-        obj: { path: [path.split('-')[0]], pathParam: path.split('-')[1], title: action.payload.title, subCategories: [], tasks: [] } 
-    }));
+function* addCategoryWatcher() {
+  yield takeEvery('START_ADD_CATEGORY_PROCESS', addCategoryGen);
 }
+
+export default addCategoryWatcher;

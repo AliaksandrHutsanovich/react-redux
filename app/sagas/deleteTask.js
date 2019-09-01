7cbd8405a0e50561b3a3f28df72bf18b6f94b705
Sanderas <guts-817@rambler.ru>
2019-09-01 14:40:17 +0300

    fix: fix eslint validation in file deleteTask.js

--- app/sagas/deleteTask.js
+++ app/sagas/deleteTask.js
@@ -2,16 +2,19 @@ import { takeEvery, put, select } from 'redux-saga/effects';
 import { insertTask, deleteTask, addToUnDo } from '../actions/actions';
 import { getEntity } from '../selectors/selectorsForEntities';
 
-export function* deleteTaskWatcher() {
-    yield takeEvery('START_DELETE_TASK_PROCESS', deleteTaskGen);
+function* deleteTaskGen(action) {
+  const task = yield select(getEntity(action.payload.path, action.payload.pathParam));
+  yield put(deleteTask({ path: action.payload.path, pathParam: action.payload.pathParam }));
+  yield put(addToUnDo({
+    undoOperation: insertTask,
+    redoOperation: deleteTask,
+    obj: task,
+  }));
 }
 
-function* deleteTaskGen(action) {
-    let task = yield select(getEntity(action.payload.path, action.payload.pathParam));
-    yield put(deleteTask({ path: action.payload.path, pathParam: action.payload.pathParam }));
-    yield put(addToUnDo({
-        undoOperation: insertTask,
-        redoOperation: deleteTask,
-        obj: task
-    }));
+
+function* deleteTaskWatcher() {
+  yield takeEvery('START_DELETE_TASK_PROCESS', deleteTaskGen);
 }
+
+export default deleteTaskWatcher;

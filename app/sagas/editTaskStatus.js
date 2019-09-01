d07e1823ee0bfd817bcecd38736e9fdf3eb88a8d
Sanderas <guts-817@rambler.ru>
2019-09-01 14:55:45 +0300

    fix: fix eslint validation editTaskStatus.js

--- app/sagas/editTaskStatus.js
+++ app/sagas/editTaskStatus.js
@@ -1,16 +1,25 @@
-import { takeEvery, put, select } from 'redux-saga/effects';
+import { takeEvery, put } from 'redux-saga/effects';
 import { editTaskStatus, addToUnDo } from '../actions/actions';
-import { getEntity } from '../selectors/selectorsForEntities';
-
-export function* editTaskStatusWatcher() {
-    yield takeEvery('START_EDIT_TASK_STATUS_PROCESS', editTaskStatusGen);
-}
 
 function* editTaskStatusGen(action) {
-    yield put(editTaskStatus({ path: action.payload.path, pathParam: action.payload.pathParam, value: action.payload.value }));
-    yield put(addToUnDo({
-        undoOperation: editTaskStatus,
-        redoOperation: editTaskStatus,
-        obj: { path: action.payload.path, pathParam: action.payload.pathParam, value: action.payload.value }
-    }));
+  yield put(editTaskStatus({
+    path: action.payload.path,
+    pathParam: action.payload.pathParam,
+    value: action.payload.value,
+  }));
+  yield put(addToUnDo({
+    undoOperation: editTaskStatus,
+    redoOperation: editTaskStatus,
+    obj: {
+      path: action.payload.path,
+      pathParam: action.payload.pathParam,
+      value: action.payload.value,
+    },
+  }));
 }
+
+function* editTaskStatusWatcher() {
+  yield takeEvery('START_EDIT_TASK_STATUS_PROCESS', editTaskStatusGen);
+}
+
+export default editTaskStatusWatcher;

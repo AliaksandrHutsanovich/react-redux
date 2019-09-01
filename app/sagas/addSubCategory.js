17ddc9d8dd7b2b1d3efa33792fb0d6364e6e6dfb
Sanderas <guts-817@rambler.ru>
2019-09-01 13:35:21 +0300

    fix: fix eslint validation in file app/sagas/addSubCategory.js

--- app/sagas/addSubCategory.js
+++ app/sagas/addSubCategory.js
@@ -2,16 +2,24 @@ import { takeEvery, put, select } from 'redux-saga/effects';
 import { addSubCategory, deleteCategory, addToUnDo } from '../actions/actions';
 import { getSubCategoryPath } from '../selectors/selectorsForPaths';
 
-export function* addSubCategoryWatcher() {
-    yield takeEvery('START_ADD_SUB_CATEGORY_PROCESS', addSubCategoryGen);
+function* addSubCategoryGen(action) {
+  yield put(addSubCategory({ path: action.payload.path, title: action.payload.title }));
+  const pathParameter = yield select(getSubCategoryPath(action.payload.path));
+  yield put(addToUnDo({
+    undoOperation: deleteCategory,
+    redoOperation: addSubCategory,
+    obj: {
+      path: action.payload.path,
+      pathParam: pathParameter,
+      title: action.payload.title,
+      subCategories: [],
+      tasks: [],
+    },
+  }));
 }
 
-function* addSubCategoryGen(action) {
-    yield put(addSubCategory({ path: action.payload.path, title: action.payload.title }));
-    let pathParam = yield select(getSubCategoryPath(action.payload.path));
-    yield put(addToUnDo({ 
-        undoOperation: deleteCategory, 
-        redoOperation: addSubCategory, 
-        obj: { path: action.payload.path, pathParam: pathParam, title: action.payload.title, subCategories: [], tasks: [] } 
-    }));
+function* addSubCategoryWatcher() {
+  yield takeEvery('START_ADD_SUB_CATEGORY_PROCESS', addSubCategoryGen);
 }
+
+export default addSubCategoryWatcher;

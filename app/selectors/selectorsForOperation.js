b40bd0d5d20e351779d7a0a49fa0376ae036e763
Sanderas <guts-817@rambler.ru>
2019-09-01 18:01:37 +0300

    fix: fix eslint validation in file selectorsForOperations.js

--- app/selectors/selectorsForOperation.js
+++ app/selectors/selectorsForOperation.js
@@ -1,25 +1,69 @@
-export const getUndoOperation = state => state.unDoReducer.getIn(['undoOperations', state.unDoReducer.get('undoOperations').toArray().length - 1, 'undoOperation']); 
+export const getUndoOperation = (state) => state
+  .unDoReducer.getIn([
+    'undoOperations',
+    state.unDoReducer
+      .get('undoOperations').toArray().length - 1,
+    'undoOperation',
+  ]);
 
-export const getRedoOperation = state => state.reDoReducer.getIn(['redoOperations', state.reDoReducer.get('redoOperations').toArray().length - 1, 'redoOperation']);
+export const getRedoOperation = (state) => state
+  .reDoReducer.getIn([
+    'redoOperations',
+    state.reDoReducer
+      .get('redoOperations').toArray().length - 1,
+    'redoOperation',
+  ]);
 
-export const getObjFromUndoOperation = state => {
-    let obj = state.unDoReducer.getIn(['undoOperations', state.unDoReducer.get('undoOperations').toArray().length - 1, 'obj']);
-    obj.value = !obj.value;
-    return obj;
-}
+export const getObjFromUndoOperation = (state) => {
+  const obj = state.unDoReducer
+    .getIn([
+      'undoOperations',
+      state.unDoReducer
+        .get('undoOperations').toArray().length - 1,
+      'obj',
+    ]);
+  obj.value = !obj.value;
+  return obj;
+};
 
-export const getObjFromRedoOperation = state => { 
-    let obj = state.reDoReducer.getIn(['redoOperations', state.reDoReducer.get('redoOperations').toArray().length - 1, 'obj']);
-    obj.value = !obj.value;
-    return obj;
-}
+export const getObjFromRedoOperation = (state) => {
+  const obj = state.reDoReducer
+    .getIn([
+      'redoOperations',
+      state.reDoReducer
+        .get('redoOperations').toArray().length - 1,
+      'obj',
+    ]);
+  obj.value = !obj.value;
+  return obj;
+};
 
+export const getUndoOperationFromRedo = (state) => state
+  .reDoReducer.getIn([
+    'redoOperations',
+    state.reDoReducer
+      .get('redoOperations').toArray().length - 1,
+    'undoOperation',
+  ]);
 
-export const getUndoOperationFromRedo = state => state.reDoReducer.getIn(['redoOperations', state.reDoReducer.get('redoOperations').toArray().length - 1, 'undoOperation']);
+export const getReDoOperationFromUndo = (state) => state
+  .unDoReducer.getIn([
+    'undoOperations',
+    state.unDoReducer
+      .get('undoOperations').toArray().length - 1,
+    'redoOperation',
+  ]);
 
-export const getReDoOperationFromUndo = state => state.unDoReducer.getIn(['undoOperations', state.unDoReducer.get('undoOperations').toArray().length - 1, 'redoOperation']);
+export const getObjWrapperFromUndoOperation = (state) => state
+  .unDoReducer.getIn([
+    'undoOperations',
+    state.unDoReducer.get('undoOperations')
+      .toArray().length - 1,
+  ]);
 
-
-export const getObjWrapperFromUndoOperation = state => state.unDoReducer.getIn(['undoOperations', state.unDoReducer.get('undoOperations').toArray().length - 1]);
-
-export const getObjWrapperFromRedoOperation = state => state.reDoReducer.getIn(['redoOperations', state.reDoReducer.get('redoOperations').toArray().length - 1]);
+export const getObjWrapperFromRedoOperation = (state) => state
+  .reDoReducer.getIn([
+    'redoOperations',
+    state.reDoReducer.get('redoOperations')
+      .toArray().length - 1,
+  ]);

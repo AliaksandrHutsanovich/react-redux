0b37789253041f03a8244f14f0ca7d9d1cdf79b7
Sanderas <guts-817@rambler.ru>
2019-09-03 23:06:01 +0300

    feat: refactor to functional component

--- app/hightOrderComponents/addForm.js
+++ app/hightOrderComponents/addForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import { connect } from 'react-redux';
 import { Input, Button } from 'antd';
@@ -6,50 +6,40 @@ import PropTypes from 'prop-types';
 import { clearReDo } from '../actions/actions';
 import { caseClickHandlers, kindsOfAddActions } from './utils/utils';
 
-function addForm(placeholder) {
-  class AddForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { value: '' };
-      this.changeValue = this.changeValue.bind(this);
-      this.clickButton = this.clickButton.bind(this);
-    }
+const addForm = (placeholder) => {
+  const AddForm = (props) => {
+    const { url, dispatch } = props;
+    const [value, setValue] = useState('');
+    const addAction = kindsOfAddActions[placeholder];
 
-    changeValue(event) {
-      this.setState({ value: event.target.value });
-    }
+    const changeValue = (event) => {
+      setValue(event.target.value);
+    };
 
-    clickButton(addAction, dispatch) {
-      const { value } = this.state;
-      const { url } = this.props;
+    const clickButton = () => {
       dispatch(clearReDo());
       caseClickHandlers[placeholder](dispatch, value, addAction, url);
-      this.setState({ value: '' });
-    }
+      setValue('');
+    };
 
-    render() {
-      const { dispatch } = this.props;
-      const { value } = this.state;
-      const addAction = kindsOfAddActions[placeholder];
-      return (
-        <div className="form_wrapper">
-          <Input
-            placeholder={placeholder}
-            className="input"
-            value={value}
-            onChange={this.changeValue}
-          />
-          <Button
-            type="primary"
-            className="button"
-            onClick={() => { this.clickButton(addAction, dispatch); }}
-          >
-            Add
-          </Button>
-        </div>
-      );
-    }
-  }
+    return (
+      <div className="form_wrapper">
+        <Input
+          placeholder={placeholder}
+          className="input"
+          value={value}
+          onChange={changeValue}
+        />
+        <Button
+          type="primary"
+          className="button"
+          onClick={() => { clickButton(addAction); }}
+        >
+          Add
+        </Button>
+      </div>
+    );
+  };
 
   AddForm.propTypes = {
     dispatch: PropTypes.func.isRequired,
@@ -61,6 +51,6 @@ function addForm(placeholder) {
   };
 
   return connect()(AddForm);
-}
+};
 
 export default addForm;

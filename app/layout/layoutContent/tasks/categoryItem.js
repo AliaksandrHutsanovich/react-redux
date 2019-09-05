ef0c2d03c886afc964bb648f6cff96f4d1725358
Sanderas <guts-817@rambler.ru>
2019-09-05 22:25:17 +0300

    feat: refactor categoryItem for functional component

--- app/layout/layoutContent/tasks/categoryItem.js
+++ app/layout/layoutContent/tasks/categoryItem.js
@@ -1,22 +1,12 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
-class CategoryItem extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    const { title } = this.props;
-    return (nextProps.title !== title);
-  }
-
-  render() {
-    const { title } = this.props;
-    return (
-      <p className="item-text">{title}</p>
-    );
-  }
-}
+const CategoryItem = ({ title }) => (
+  <p className="item-text">{title}</p>
+);
 
 CategoryItem.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
-export default CategoryItem;
+export default memo(CategoryItem);

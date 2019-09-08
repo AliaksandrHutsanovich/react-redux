2af42d61d98da823d8ccedc458f596fc78723a57
Sanderas <guts-817@rambler.ru>
2019-09-08 14:58:16 +0300

    feat: change tasks.js to functional component

--- app/layout/layoutContent/tasks/tasks.js
+++ app/layout/layoutContent/tasks/tasks.js
@@ -1,6 +1,6 @@
 /* eslint react/forbid-prop-types: 0 */
 
-import React from 'react';
+import React, { useEffect, memo } from 'react';
 import 'antd/dist/antd.css';
 import { List } from 'antd';
 import { connect } from 'react-redux';
@@ -9,74 +9,56 @@ import { countPercentage } from '../../../actions/actions';
 import Task from './task';
 
 
-class Tasks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {
-    const { dispatch, tasks } = this.props;
+const Tasks = ({
+  dispatch,
+  tasks,
+  showDone,
+  searchKey,
+  match,
+}) => {
+  useEffect(() => {
     dispatch(
       countPercentage({
         done: tasks.filter((task) => task.isFinished).length,
         all: tasks.length,
       }),
     );
-  }
+  });
 
-  static getDerivedStateFromProps(props) {
-    const { dispatch, tasks } = props;
-    dispatch(
-      countPercentage({
-        done: tasks.filter((task) => task.isFinished).length,
-        all: tasks.length,
-      }),
-    );
-    return {};
-  }
-
-  render() {
-    const {
-      showDone,
-      searchKey,
-      tasks,
-      match: { url },
-    } = this.props;
-    let data = tasks || [];
-    data = data
-      .map((elem, index) => {
-        const newElem = elem;
-        newElem.index = index;
-        return newElem;
-      })
-      .filter((elem) => elem.isFinished === showDone)
-      .filter((elem) => elem.title.match(searchKey));
-    return (
-      <div>
-        <List
-          itemLayout="horizontal"
-          dataSource={data}
-          className="list"
-          split={false}
-          renderItem={
-            (item) => (
-              <List.Item className="list-item">
-                <Task
-                  index={item.index}
-                  url={url}
-                  title={item.title}
-                  description={item.Description}
-                  isFinished={item.isFinished}
-                />
-              </List.Item>
-            )
-          }
-        />
-      </div>
-    );
-  }
-}
+  const { url } = match;
+  let data = tasks || [];
+  data = data
+    .map((elem, index) => {
+      const newElem = elem;
+      newElem.index = index;
+      return newElem;
+    })
+    .filter((elem) => elem.isFinished === showDone)
+    .filter((elem) => elem.title.match(searchKey));
+  return (
+    <div>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        className="list"
+        split={false}
+        renderItem={
+          (item) => (
+            <List.Item className="list-item">
+              <Task
+                index={item.index}
+                url={url}
+                title={item.title}
+                description={item.Description}
+                isFinished={item.isFinished}
+              />
+            </List.Item>
+          )
+        }
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = (state, props) => ({
   tasks: state.actionReducers
@@ -93,4 +75,4 @@ Tasks.propTypes = {
   match: PropTypes.object.isRequired,
 };
 
-export default connect(mapStateToProps)(Tasks);
+export default connect(mapStateToProps)(memo(Tasks));

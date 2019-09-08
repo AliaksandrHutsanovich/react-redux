7f35bb518950a5fa1c8987bcbdf14d2467b00bc3
Sanderas <guts-817@rambler.ru>
2019-09-08 17:18:33 +0300

    fix: fix eslint validation and add historyApiFallback

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,39 @@
-let path = require('path');
+const path = require('path');
 
 module.exports = {
-    entry: "./app/app.js",
-    output: {
-        path: path.resolve(__dirname, './public'),
-        publicPath: '/public/',
-        filename: "bundle.js"
-    },
-    devtool: "#sourcemap",
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules)/,
-                loader: "babel-loader",
-                options: {
-                    presets: ["env", "react", "es2015"],
-                    plugins: [
-                        "transform-object-rest-spread",
-                        ["transform-class-properties", { "spec": true }],
-                        ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }],
-                        ["transform-runtime", {
-                            "polyfill": false,
-                            "regenerator": true
-                        }],
-                        "babel-plugin-syntax-dynamic-import"
-                    ]
-                }
-            },
-            {
-                test: /\.css$/,
-                loaders: 'style-loader!css-loader'
-            }
-        ]
-    }
-}
+  entry: './app/app.js',
+  output: {
+    path: path.resolve(__dirname, './public'),
+    publicPath: '/public/',
+    filename: 'bundle.js',
+  },
+  devtool: '#sourcemap',
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react', 'es2015'],
+          plugins: [
+            'transform-object-rest-spread',
+            ['transform-class-properties', { spec: true }],
+            ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+            ['transform-runtime', {
+              polyfill: false,
+              regenerator: true,
+            }],
+            'babel-plugin-syntax-dynamic-import',
+          ],
+        },
+      }, {
+        test: /\.css$/,
+        loaders: 'style-loader!css-loader',
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+};
